75be3fce5e800610aa1e391a9cead4099ee2ccd8
sudipizme32 <poudelsp32@gmail.com>
2019-09-25 15:56:13 +0930

    My Salon with DAL implemented.

---
+++ MySalon-with DAL/DAL/Migrations/20190925054021_tests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DAL.Migrations
+{
+    public partial class tests : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Appointment2",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Appointment2", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Appointment2");
+        }
+    }
+}
---
+++ MySalon-with DAL/MySalon-Master/Models/Appointment.cs
@@ -0,0 +1,10 @@
+﻿namespace MySalon_Master.Models
+{
+    public class Appointment
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Date { get; set; }
+    }
+}
